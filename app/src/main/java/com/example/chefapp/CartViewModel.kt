12400cc6a63d8fd35fdb78916f0a4c1f32b964ff
Piotr Wiaderek <regularuseless@gmail.com>
2025-01-29 18:22:42 +0100

    cart implementation

---
+++ app/src/main/java/com/example/chefapp/CartViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.chefapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel()
+{
+    private val _ingredients = MutableLiveData<List<Ingredient>>(mutableListOf())
+    val ingredients: LiveData<List<Ingredient>> get() = _ingredients
+
+    fun addIngredients(newIngredients: List<Ingredient>) {
+        val currentList = _ingredients.value.orEmpty().toMutableList()
+        for (newIngredient in newIngredients) {
+            val existingIngredient = currentList.find { it.name == newIngredient.name }
+
+            if (existingIngredient != null) {
+                existingIngredient.amount += newIngredient.amount
+            } else {
+                currentList.add(newIngredient)
+            }
+        }
+        _ingredients.value = currentList
+    }
+}
